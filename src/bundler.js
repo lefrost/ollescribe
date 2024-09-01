1ee185ca6661189e2b13ec2e152d31eea49c3aef
lé <elcasaboxi@gmail.com>
2024-09-01 08:53:49 +0700

    init

---
+++ src/bundler.js
@@ -0,0 +1,8 @@
+// import Ollesvege from "./lib/pkg/Ollesvege.svelte";
+
+// const target = document.querySelector("#ollesvege");
+
+// if (target) {
+// 	// eslint-disable-next-line no-new
+// 	new Ollesvege({ target });
+// }
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+	plugins: [
+		svelte({
+			emitCss: false,
+		}),
+	],
+
+	build: {
+		assetsDir: "",
+		sourcemap: true,
+		lib: {
+			entry: "src/bundler.js",
+			formats: ["iife"],
+			name: "Ollesvege",
+			fileName: "ollesvege",
+		},
+	},
+});
